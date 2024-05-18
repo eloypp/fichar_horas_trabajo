7d248527f0daa4b95fd91dc9b86a6bbb35280768
eloypp <eloypp99@gmail.com>
2024-05-18 22:10:49 +0100

    Vínculo Firebase
    Configuración hecha para vincular la aplicación a Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:provider/provider.dart';
 import '../providers/timeTracker.dart';
 import '../vistas/home.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(
     MultiProvider(
       providers: [
